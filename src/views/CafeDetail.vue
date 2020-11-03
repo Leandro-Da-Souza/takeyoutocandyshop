fcfd200106be46f295abe835ddb7a7b24b85d5a7
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-11-03 13:44:45 +0100

    add about section

--- src/views/CafeDetail.vue
+++ src/views/CafeDetail.vue
@@ -10,6 +10,28 @@
               <p>&rarr;</p>
           </div>
       </section>
+      <section class="about">
+          <div class="top">
+              <h2>Mr Cupcake</h2>
+              <img src="https://via.placeholder.com/52x16" alt="" class="ratings">
+          </div>
+          <p>Jawi, Pasuruan</p>
+          <div class="rewards">
+              <div class="left">
+                  <p>Check the rewards for this cafe!</p>
+                  <a href="/">16 Rewards</a>
+              </div>
+              <div class="right">
+                  <p>&gt;</p>
+              </div>
+          </div>
+      </section>
+      <section class="selections">
+
+      </section>
+      <section class="cards">
+          <div class="card"></div>
+      </section>
   </div>
 </template>
 
@@ -49,15 +71,50 @@ export default {
         background-color: base.$red
         border: none
     p
-        color: #000
+        // color: #000
         padding-top: 8px
     p:first-of-type
         margin: 0 50px 0 10px
     p:nth-of-type(2)
         margin-top: -10px
         font-size: 2rem
+        color: #000
 
+.about
+    max-width: 320px
+    margin-top: 10px
+    .top 
+        display: flex
+        justify-content: space-between
+        .ratings
+            width: 52px
+            height: 16px
+            margin: 5px
+        h2
+            color: #000
 
+    .rewards
+        width: 327px
+        height: 58px
+        background-color: #fafafa
+        display: flex
+        justify-content: space-between
+        border-radius: 10px
+        .left 
+            p
+                color: #000
+                font-size: 0.9rem
+            a
+                color: base.$light-blue
+                text-decoration: none
+                font-size: 0.8rem
+        .right
+            p
+                font-size: 1.5rem
+                color: #000
+                padding-right: 10px
+        .right, .left 
+            padding-top: 10px
 
 
 </style>
