12fa16a0784db7c9a72349151c66331a882bfcba
Leandro Da Souza <dasouza.leandro@gmail.com>
2020-11-03 13:12:55 +0100

    add cafe img and location element

--- src/views/CafeDetail.vue
+++ src/views/CafeDetail.vue
@@ -3,6 +3,13 @@
       <header>
           <router-link to="/" tag="h3">&lt;</router-link>
       </header>
+      <section class="img-container">
+          <div class="location">
+              <span class="map-icon"></span>
+              <p>See location on map</p>
+              <p>&rarr;</p>
+          </div>
+      </section>
   </div>
 </template>
 
@@ -14,5 +21,43 @@ export default {
 
 <style lang='sass' scoped>
 @use '@/styles/base.sass'
+
+.img-container
+    background-image: url('https://images.pexels.com/photos/1402407/pexels-photo-1402407.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')
+    background-repeat: no-repeat
+    background-position: center
+    background-size: cover
+    position: relative 
+    width: 327px
+    height: 327px
+    border-radius: 16px
+
+.location
+    background-color: #fff
+    position: absolute
+    width: 290px
+    height: 54px
+    display: flex
+    border-radius: 27px
+    margin: 10px 0 0 10px
+    padding-top: 5px
+    padding-left: 10px
+    .map-icon 
+        border-radius: 50%
+        width: 40px
+        height: 40px
+        background-color: base.$red
+        border: none
+    p
+        color: #000
+        padding-top: 8px
+    p:first-of-type
+        margin: 0 50px 0 10px
+    p:nth-of-type(2)
+        margin-top: -10px
+        font-size: 2rem
+
+
+
 
 </style>
