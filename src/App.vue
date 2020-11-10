679bf2e72a1669498ecc9be63e9278b39eb0aa56
jensen-education-razieh-rouzbahani <razie.rouzbahani@gmail.com>
2020-11-10 16:57:55 +0100

    remove routes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/cartdetail">Cart Detail</router-link> |
-      <router-link to="/productdetail">Product Detail</router-link> |
-      <router-link to="/cafedetail">Cafe Detail</router-link> 
+      <router-link to="/">Home</router-link> 
+      <!-- <router-link to="/about">About</router-link>  -->
+      <!-- <router-link to="/cartdetail">Cart Detail</router-link> | -->
+      <!-- <router-link to="/productdetail">Product Detail</router-link> | -->
+      <!-- <router-link to="/cafedetail">Cafe Detail</router-link>  -->
     </div>
     <router-view/>
   </div>
